fac27a66646087157d91aeac597127baf2109474
Puranjanaaa <puranja@gmail.com>
2022-04-18 16:32:14 +0530

    Pay1

---
+++ Payment_Management/src/main/java/com/payment/Payment_Management/Payment.java
@@ -0,0 +1,26 @@
+package com.payment.Payment_Management;
+
+public class Payment {
+	private String name;
+	private String accNo;
+	private String dueDate;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+}
