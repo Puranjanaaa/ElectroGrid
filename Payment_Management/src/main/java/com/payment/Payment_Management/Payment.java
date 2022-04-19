d9edc15493968b48ec262017f8b9be6e02a848d6
Puranjanaaa <puranja@gmail.com>
2022-04-19 08:27:16 +0530

    Payment 2

--- Payment_Management/src/main/java/com/payment/Payment_Management/Payment.java
+++ Payment_Management/src/main/java/com/payment/Payment_Management/Payment.java
@@ -1,14 +1,41 @@
 package com.payment.Payment_Management;
 
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
 public class Payment {
-	private String name;
+	private String units;
 	private String accNo;
 	private String dueDate;
-	public String getName() {
-		return name;
+	private String amount;
+	private String pricePerUnit;
+	private String additionalUnit;
+	
+	
+	public String getAmount() {
+		return amount;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getPricePerUnit() {
+		return pricePerUnit;
+	}
+	public void setPricePerUnit(String pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+	public String getAdditionalUnit() {
+		return additionalUnit;
+	}
+	public void setAdditionalUnit(String additionalUnit) {
+		this.additionalUnit = additionalUnit;
+	}
+	public String getUnits() {
+		return units;
+	}
+	public void setUnits(String units) {
+		this.units = units;
 	}
 	public String getAccNo() {
 		return accNo;
